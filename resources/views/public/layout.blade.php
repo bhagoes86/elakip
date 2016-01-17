bd96015dd57e194471569379e19f376f8b7c3d2c
Fathur Rohman <fathur_rohman17@yahoo.co.id>
2016-01-17 07:20:41 +0700

    fix typo title

--- resources/views/public/layout.blade.php
+++ resources/views/public/layout.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Kementrian PUPERA</title>
+    <title>Kementerian PUPERA</title>
 
     <link href="{{asset('lib/bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet">
 
